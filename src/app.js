abb644c2935f672b630a318db64759b03d910fe0
Adam Patrick <adam_77paper@hotmail.com>
2021-11-26 00:43:51 -0500

    added 5 day forecast

--- src/app.js
+++ src/app.js
@@ -9,12 +9,36 @@ let actualTemp;
 let wind;
 let place;
 let day2;
+let day3;
+let day4;
+let day5;
+let day6;
 let desc2;
+let desc3;
+let desc4;
+let desc5;
+let desc6;
 let imgValue;
+let imgValue2;
+let imgValue3;
+let imgValue4;
+let imgValue5;
 
 const headerTemp = document.querySelector('.main-temp');
 const forcastDiv = document.querySelector('.desc');
 const imgDiv = document.querySelector('.weatherImg');
+
+const forcastDiv2 = document.querySelector('.desc2');
+const imgDiv2 = document.querySelector('.weatherImg2');
+
+const forcastDiv3 = document.querySelector('.desc3');
+const imgDiv3 = document.querySelector('.weatherImg3');
+
+const forcastDiv4 = document.querySelector('.desc4');
+const imgDiv4 = document.querySelector('.weatherImg4');
+
+const forcastDiv5 = document.querySelector('.desc5');
+const imgDiv5 = document.querySelector('.weatherImg5');
 
 function weatherCheck(imgCode) { // checking API for imgCode and selecting the appropriate img one
   let imgSrc = '';
@@ -24,7 +48,7 @@ function weatherCheck(imgCode) { // checking API for imgCode and selecting the a
   } else if (imgCode === '01d' || imgCode === '01n') {
     imgSrc = 'images/clear.png';
   } else if (imgCode === '02d' || imgCode === '02n') {
-    imgSrc = 'images/clouds.png';
+    imgSrc = 'images/partialCloud.png';
   } else if (imgCode === '03d' || imgCode === '03n') {
     imgSrc = 'images/partialCloud.png';
   } else if (imgCode === '09d' || imgCode === '09n') {
@@ -53,7 +77,8 @@ function appendCity(city) {
 
 function appendTempeture(temp) {
   const tempH2 = document.createElement('h2');
-  tempH2.textContent = `${temp}°`;
+  tempH2.textContent = `${Math.round(temp)}°`;
+  tempH2.classList.add('tempFormat');
   headerTemp.appendChild(tempH2);
 }
 
@@ -74,7 +99,7 @@ function appendForcastDescription(forDesc) {
 
 function appendForcastTemp(forTemp) {
   const forcastTemp = document.createElement('h3');
-  forcastTemp.textContent = `${forTemp}°`;
+  forcastTemp.textContent = `${Math.round(forTemp)}°`;
   forcastDiv.appendChild(forcastTemp);
 }
 
@@ -85,6 +110,110 @@ function appendImg(img) {
   const weatherImg = document.createElement('img');
   weatherImg.src = weatherCheck(img);
   imgDiv.appendChild(weatherImg);
+}
+
+// second DOM Forcast
+
+function appendForcastDescription2(forDesc) {
+  while (forcastDiv2.firstChild) {
+    forcastDiv2.removeChild(forcastDiv2.firstChild);
+  }
+  const forcastDesc = document.createElement('h3');
+  forcastDesc.textContent = `${forDesc}`;
+  forcastDiv2.appendChild(forcastDesc);
+}
+
+function appendForcastTemp2(forTemp) {
+  const forcastTemp = document.createElement('h3');
+  forcastTemp.textContent = `${Math.round(forTemp)}°`;
+  forcastDiv2.appendChild(forcastTemp);
+}
+
+function appendImg2(img) {
+  while (imgDiv2.firstChild) {
+    imgDiv2.removeChild(imgDiv2.firstChild);
+  }
+  const weatherImg = document.createElement('img');
+  weatherImg.src = weatherCheck(img);
+  imgDiv2.appendChild(weatherImg);
+}
+
+// third DOM Forcast
+
+function appendForcastDescription3(forDesc) {
+  while (forcastDiv3.firstChild) {
+    forcastDiv3.removeChild(forcastDiv3.firstChild);
+  }
+  const forcastDesc = document.createElement('h3');
+  forcastDesc.textContent = `${forDesc}`;
+  forcastDiv3.appendChild(forcastDesc);
+}
+
+function appendForcastTemp3(forTemp) {
+  const forcastTemp = document.createElement('h3');
+  forcastTemp.textContent = `${Math.round(forTemp)}°`;
+  forcastDiv3.appendChild(forcastTemp);
+}
+
+function appendImg3(img) {
+  while (imgDiv3.firstChild) {
+    imgDiv3.removeChild(imgDiv3.firstChild);
+  }
+  const weatherImg = document.createElement('img');
+  weatherImg.src = weatherCheck(img);
+  imgDiv3.appendChild(weatherImg);
+}
+
+// fourth DOM Forcast
+
+function appendForcastDescription4(forDesc) {
+  while (forcastDiv4.firstChild) {
+    forcastDiv4.removeChild(forcastDiv4.firstChild);
+  }
+  const forcastDesc = document.createElement('h3');
+  forcastDesc.textContent = `${forDesc}`;
+  forcastDiv4.appendChild(forcastDesc);
+}
+
+function appendForcastTemp4(forTemp) {
+  const forcastTemp = document.createElement('h3');
+  forcastTemp.textContent = `${Math.round(forTemp)}°`;
+  forcastDiv4.appendChild(forcastTemp);
+}
+
+function appendImg4(img) {
+  while (imgDiv4.firstChild) {
+    imgDiv4.removeChild(imgDiv4.firstChild);
+  }
+  const weatherImg = document.createElement('img');
+  weatherImg.src = weatherCheck(img);
+  imgDiv4.appendChild(weatherImg);
+}
+
+// fifth DOM Forcast
+
+function appendForcastDescription5(forDesc) {
+  while (forcastDiv5.firstChild) {
+    forcastDiv5.removeChild(forcastDiv5.firstChild);
+  }
+  const forcastDesc = document.createElement('h3');
+  forcastDesc.textContent = `${forDesc}`;
+  forcastDiv5.appendChild(forcastDesc);
+}
+
+function appendForcastTemp5(forTemp) {
+  const forcastTemp = document.createElement('h3');
+  forcastTemp.textContent = `${Math.round(forTemp)}°`;
+  forcastDiv5.appendChild(forcastTemp);
+}
+
+function appendImg5(img) {
+  while (imgDiv5.firstChild) {
+    imgDiv5.removeChild(imgDiv5.firstChild);
+  }
+  const weatherImg = document.createElement('img');
+  weatherImg.src = weatherCheck(img);
+  imgDiv5.appendChild(weatherImg);
 }
 
 async function getCurrentWeather() {
@@ -125,6 +254,30 @@ async function getForcastWeather() {
     appendImg(imgValue);
     appendForcastDescription(desc2);
     appendForcastTemp(day2);
+    day3 = weatherData.list[14].main.temp;
+    desc3 = weatherData.list[14].weather[0].description;
+    imgValue2 = weatherData.list[14].weather[0].icon;
+    appendImg2(imgValue2);
+    appendForcastDescription2(desc3);
+    appendForcastTemp2(day3);
+    day4 = weatherData.list[22].main.temp;
+    desc4 = weatherData.list[22].weather[0].description;
+    imgValue3 = weatherData.list[22].weather[0].icon;
+    appendImg3(imgValue3);
+    appendForcastDescription3(desc4);
+    appendForcastTemp3(day4);
+    day5 = weatherData.list[30].main.temp;
+    desc5 = weatherData.list[30].weather[0].description;
+    imgValue4 = weatherData.list[30].weather[0].icon;
+    appendImg4(imgValue4);
+    appendForcastDescription4(desc5);
+    appendForcastTemp4(day5);
+    day6 = weatherData.list[39].main.temp;
+    desc6 = weatherData.list[39].weather[0].description;
+    imgValue5 = weatherData.list[39].weather[0].icon;
+    appendImg5(imgValue5);
+    appendForcastDescription5(desc6);
+    appendForcastTemp5(day6);
   } catch {
     console.log('AYO');
   }
